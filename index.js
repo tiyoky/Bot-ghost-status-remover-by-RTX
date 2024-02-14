6ee8fa28d5596fb075ba8bb11e7f5a2b894ca807
tiyoky <160074145+tiyoky@users.noreply.github.com>
2024-02-14 16:48:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["mde by _tiyoky ","all developed by _tiyoky","bot de _tiyoky et reufrandom"];
 
 
 let currentIndex = 0;

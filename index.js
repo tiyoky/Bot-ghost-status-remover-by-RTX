28256660c5806c8c8516679419e697d5f8e96f1d
tiyoky <160074145+tiyoky@users.noreply.github.com>
2024-02-25 12:16:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["fait par _tiyoky","made with 🩷 by tiyoky"," tekslide on top"];
 
 
 let currentIndex = 0;
